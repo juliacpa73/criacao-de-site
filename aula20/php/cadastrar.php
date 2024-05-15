f41695c3bb1b546cd699658640aa11d9d9e5a467
julia <juliacpa73@gmail.com>
2024-05-15 16:53:46 -0300

    introdução a php

---
+++ aula20/php/cadastrar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastrado!</h1>
+    <p> Olá
+    <?php 
+        $valor = 0;
+        while($valor < 6){
+            echo "<p>Olá, <dtrong>fulano $valor </strong></p>"; 
+            $valor += 1;
+        }
+        echo "Chega, né?";
+    ?>
+    <?php 
+        $nome = $_GET['nome'];
+        echo "<p>Cadastrado $nome</p>"; //a variável $nome está pegando com o método $_GET o id do nome digitado pelo usuário no formulário
+    ?>
+        <?php 
+        $email = $_POST['email'];
+        echo "<p>Olá, $email</p>"; //método post permite que os dados do formulario não apareça na url
+    </p>
+</body>
+</html>
